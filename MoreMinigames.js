07a4d13b59a01ad3fb7c14419b95cff0d09e3f80
Moonworks64 <ipblnc310@gmail.com>
2026-08-06 20:48:22 +1000

    Remove comment

--- MoreMinigames.js
+++ MoreMinigames.js
@@ -1,12 +1,5 @@
 //EXAMPLE MOD
 Game.registerMod('MoonworksMoreMinigames',{
-    /*
-        what this example mod does:
-        -double your CpS
-        -display a little popup for half a second whenever you click the big cookie
-        -add a little intro text above your bakery name, and generate that intro text at random if you don't already have one
-        -save and load your intro text
-    */
     init:function(){
         Game.registerHook('click', function() {
             Game.gainBuff('blood frenzy', Math.ceil(1), 666);
